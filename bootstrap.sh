e4e0b6c040706d2002135fc75e35abca6f7d2ceb
Yuhao Zhang <yhzhang1994@gmail.com>
2020-03-30 17:00:10 -0700

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y \
     gnupg-agent \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+sudo add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
