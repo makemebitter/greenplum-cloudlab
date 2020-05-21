68451b7cf11b405ad6996ae671d1ac641b566846
Yuhao Zhang <yhzhang1994@gmail.com>
2020-05-21 16:37:22 -0700

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,25 +76,23 @@ sudo rsync -av /home/ /mnt/home/
 sudo rm -rvf /home/*
 sudo mount -o bind /mnt/home/ /home/
 
-sudo mkdir /mnt/usr.lib
-sudo rsync -av /usr/lib/ /mnt/usr.lib/
-sudo rm -rvf /usr/lib/*
-mount -o bind /mnt/usr.lib/ /usr/lib/
+
 
 sudo mkdir /mnt/var.lib
 sudo rsync -av /var/lib/ /mnt/var.lib/
 sudo rm -rvf /var/lib/*
-mount -o bind /mnt/var.lib/ /var/lib/
+sudo mount -o bind /mnt/var.lib/ /var/lib/
 
 sudo mkdir /mnt/var.cache
 sudo rsync -av /var/cache/ /mnt/var.cache/
 sudo rm -rvf /var/cache/*
-mount -o bind /mnt/var.cache/ /var/cache/
+sudo mount -o bind /mnt/var.cache/ /var/cache/
 
 sudo mkdir /mnt/usr.local
 sudo rsync -av /usr/local/ /mnt/usr.local/
 sudo rm -rvf /usr/local/*
-mount -o bind /mnt/usr.local/ /usr/local/
+sudo mount -o bind /mnt/usr.local/ /usr/local/
+
 
 sudo dpkg --configure -a
 # greenplum
@@ -170,38 +168,41 @@ echo "/usr/local/cuda/extras/CUPTI/lib64" | sudo tee -a /etc/ld.so.conf
 sudo rm -rvf /usr/lib/python2.7/dist-packages/OpenSSL
 sudo pip install -U pyopenssl
 sudo pip install --upgrade pip
+
+
+# CUDA 10.0
 # Add NVIDIA package repositories
 # Add HTTPS support for apt-key
 
-sudo apt-get install -y gnupg-curl
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt-get update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
-sudo apt install -y ./nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
-sudo apt-get update
+# sudo apt-get install -y gnupg-curl
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+# sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+# sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+# sudo apt-get update
+# wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
+# sudo apt install -y ./nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
+# sudo apt-get update
 
 
 
-# Install NVIDIA driver
-# Issue with driver install requires creating /usr/lib/nvidia
-sudo mkdir /usr/lib/nvidia
-sudo apt-get install -y --no-install-recommends nvidia-410
-# Reboot. Check that GPUs are visible using the command: nvidia-smi
+# # Install NVIDIA driver
+# # Issue with driver install requires creating /usr/lib/nvidia
+# sudo mkdir /usr/lib/nvidia
+# sudo apt-get install -y --no-install-recommends nvidia-410
+# # Reboot. Check that GPUs are visible using the command: nvidia-smi
 
-# Install development and runtime libraries (~4GB)
-sudo apt-get install -y --no-install-recommends \
-    cuda-10-0 \
-    libcudnn7=7.4.1.5-1+cuda10.0  \
-    libcudnn7-dev=7.4.1.5-1+cuda10.0
+# # Install development and runtime libraries (~4GB)
+# sudo apt-get install -y --no-install-recommends \
+#     cuda-10-0 \
+#     libcudnn7=7.4.1.5-1+cuda10.0  \
+#     libcudnn7-dev=7.4.1.5-1+cuda10.0
 
 
-# Install TensorRT. Requires that libcudnn7 is installed above.
-sudo apt-get update && \
-        sudo apt-get install nvinfer-runtime-trt-repo-ubuntu1604-5.0.2-ga-cuda10.0 \
-        && sudo apt-get update \
-        && sudo apt-get install -y --no-install-recommends libnvinfer5=5.0.2-1+cuda10.0 libnvinfer-dev=5.0.2-1+cuda10.0
+# # Install TensorRT. Requires that libcudnn7 is installed above.
+# sudo apt-get update && \
+#         sudo apt-get install nvinfer-runtime-trt-repo-ubuntu1604-5.0.2-ga-cuda10.0 \
+#         && sudo apt-get update \
+#         && sudo apt-get install -y --no-install-recommends libnvinfer5=5.0.2-1+cuda10.0 libnvinfer-dev=5.0.2-1+cuda10.0
 
 
 # # CUDA 10.1
