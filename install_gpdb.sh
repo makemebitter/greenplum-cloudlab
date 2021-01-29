95cecd0be9963e4d2afe2d47cc8e84a45c53f4f0
Yuhao Zhang <yhzhang1994@gmail.com>
2021-01-29 13:39:03 -0800

    update

--- install_gpdb.sh
+++ install_gpdb.sh
@@ -135,6 +135,8 @@ if [ "$duty" = "m" ]; then
             sleep 5s
         fi
     done
+    gpstop -a -M fast
+    gpstart -a
     echo "GPDB INITIALIZATION FINISHED"    
 fi
 touch $TAG_PATH
--- setup.sh
+++ setup.sh
@@ -9,35 +9,12 @@ for i in $(seq 0 7); do
   curl "http://supun.ucsd.edu/cerebro/data/imagenet/valid/valid_$i.h5" --create-dirs -o $VALID_PATH/valid_$i.h5
 done
 
-# cerebro standalone
-export CEREBRO_LOG_DIR=/mnt/nfs/logs/run_logs/cerebro_run_logs
-mkdir $CEREBRO_LOG_DIR
-unset PYTHONPATH
-export PYTHONPATH="${PYTHONPATH}:/local:/local/cerebro-greenplum/code"
-bash /local/cerebro/bin/cerebro_worker.sh start $WORKER_NAME 8000 $CEREBRO_LOG_DIR &
-
-git clone https://github.com/nurdtechie98/drive-cli.git
-# paste oauth client secret
-sudo python3.7 -m pip install -e .
-drive --remote login
 
 
 
-
-# sudo apt-get -y install git cmake build-essential libgcrypt11-dev libyajl-dev libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev pkg-config zlib1g-dev
-# git clone https://github.com/vitalif/grive2
-# cd grive2
-# dpkg-buildpackage -j4
-# cd ..
-# sudo apt install grive_0.5.1+git20160731_amd64.deb
-# cd ~
-# mkdir google-drive
-# cd google-drive
-
-
-# wget https://github.com/makemebitter/gdrive-cli-builder/suites/1300909639/artifacts/20413740
-
+wget -O /home/gpadmin/.screenrc https://raw.githubusercontent.com/makemebitter/greenplum-cloudlab/master/.screenrc 
 # Download gdrive-linux-x64
+wget https://github.com/makemebitter/gdrive-cli-builder/releases/download/1.0/gdrive-linux-x64
 sudo chmod +x gdrive-linux-x64
 sudo mv gdrive-linux-x64 /usr/local/bin/gdrive
 gdrive about
@@ -46,4 +23,39 @@ gdrive about
 
 
 # Criteo preprocessing
-python2 preprocessing_criteo.py --data_root '/mnt/criteo' --log_root '/mnt/nfs/logs' --nfs_root '/mnt/nfs/models/data_share/criteo/tfrecords'
+# python2 preprocessing_criteo.py --data_root '/mnt/hdd/criteo' --log_root '/mnt/nfs/logs' --nfs_root '/mnt/nfs/models/data_share/criteo/tfrecords'
+
+
+# Partition hdd disk (on all), check if sdb is actually the hdd
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk /dev/sdb
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+    # default - end at ending of disk
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+sudo mkfs.ext3 /dev/sdb1
+
+# on master
+mkdir -p /mnt/nfs/hdd
+sudo mount /dev/sdb1 /mnt/nfs/hdd
+# on worker
+mkdir -p /mnt/hdd
+sudo mount /dev/sdb1 /mnt/hdd
+
+# Download criteo data (on master)
+sudo chmod -R 777 /mnt/nfs
+mkdir -p /mnt/nfs/hdd/criteo
+gdrive download -r --path /mnt/nfs/hdd/criteo 1c7UapUWys474HhbNQp5iCOEMsbJn4O_2 
+
+# run cerebro standalone (on workers)
+# cerebro standalone
+export CEREBRO_LOG_DIR=/mnt/nfs/logs/run_logs/cerebro_run_logs
+mkdir $CEREBRO_LOG_DIR
+unset PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}:/local:/local/cerebro-greenplum/code"
+bash /local/cerebro/bin/cerebro_worker.sh start $WORKER_NAME 8000 $CEREBRO_LOG_DIR &
