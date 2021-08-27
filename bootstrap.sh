fb48439aea822dcfeedcc58bb58e93f4fe6471c6
Yuhao Zhang <yuz870@eng.ucsd.edu>
2021-08-27 16:21:45 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -345,8 +345,11 @@ if [ "$duty" = "m" ]; then
     # Theia
     sudo python3.7 -m pip install python-language-server flake8 autopep8
     sudo apt-get install curl
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt-get install -y nodejs
+    sudo apt-get install libx11-dev libxkbfile-dev
+    sudo apt-get install libsecret-1-dev
+    sudo apt-get install build-essential
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
     sudo apt-get update && sudo apt-get -y install yarn
